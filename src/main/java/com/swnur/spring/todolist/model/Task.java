837870817f6be3fac668b6d7bf536f27f2bb55f6
Nurbek Shukan <shukannurbek@gmail.com>
2024-11-27 19:27:28 +0100

    Create entity model for task

---
+++ src/main/java/com/swnur/spring/todolist/model/Task.java
@@ -0,0 +1,20 @@
+package com.swnur.spring.todolist.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Task {
+
+    private String headline;
+    private String description;
+    private LocalDate creationDate;
+    private LocalDate dueDate;
+    private boolean isFinished;
+
+}
